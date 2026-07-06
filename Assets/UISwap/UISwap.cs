5b74fb3ebd0a4a6fd6f3775fdabded81d62eaac2
Moritz Klock <moritz.klock@student.kit.edu>
2026-07-06 14:47:55 +0200

    Downgrade project to old Unity version

--- Assets/UISwap/UISwap.cs
+++ Assets/UISwap/UISwap.cs
@@ -7,12 +7,19 @@ public class UISwap : MonoBehaviour
 {
     public GameObject Focused;
     public GameObject Distracted;
+    public GameObject GazeTrigger;
 
     private readonly IFocusEffect focusEffect;
 
     public UISwap()
     {
-        focusEffect = new LinearEffect(this, new UnionEffect(new LogStrengthEffect(), new BackgroundColorEffect(new Color(0, 150 / 255.0f, 130 / 255.0f))));
+        focusEffect = new LinearEffect(this, new UnionEffect(new IFocusEffect[]
+        {
+            new LogStrengthEffect(),
+            new BackgroundColorEffect(new Color(0, 150 / 255.0f, 130 / 255.0f)),
+            new VignetteEffect(),
+        }
+        ));
     }
 
     public void LooksAtScreen()
---
+++ Assets/UISwap/VignetteEffect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+namespace Assets.UISwap
+{
+    internal class VignetteEffect : IFocusEffect
+    {
+        private Vignette vignette;
+
+        public VignetteEffect()
+        {
+
+        }
+        public void ApplyEffect(double strength)
+        {
+            if (vignette == null)
+            {
+                TryFindVignette();
+            }
+
+            vignette?.intensity.Override((float)strength);
+        }
+
+        private void TryFindVignette()
+        {
+            var volume = Object.FindAnyObjectByType<Volume>(FindObjectsInactive.Include);
+            if (volume != null && volume.profile.TryGet(out vignette))
+            {
+                Debug.Log("Vignette gefunden.");
+            }
+            else
+            {
+                Debug.LogError("Kein Volume oder Vignette in der Szene gefunden.");
+            }
+        }
+    }
+}
